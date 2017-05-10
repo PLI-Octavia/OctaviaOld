fbd481bcd435e09bd12f11144f3556b74f955589
alex <levy_a@vanessa-mbp-2.home>
2017-05-10 22:10:55 +0200

    add all off own model in superuser admin page

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from  .models import Student
+from  .models import Course
+from  .models import Teacher
+from  .models import Game
+from  .models import GameCourse
+from  .models import Score
+from  .models import Assignement
+from  .models import AssignementStudent
+
+
+
+admin.site.register(Student)
+admin.site.register(Course)
+admin.site.register(Teacher)
+admin.site.register(Game)
+admin.site.register(GameCourse)
+admin.site.register(Score)
+admin.site.register(Assignement)
+admin.site.register(AssignementStudent)
 
 # Register your models here.
