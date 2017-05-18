40778bad4475da0f728c1d7c616d0e72d2857fff
alex <levy_a@vanessa-mbp-2.home>
2017-05-18 22:57:43 +0200

    Issue #2 : beginning of django auth

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from .views import teacher
 
 urlpatterns = [
-	url(r'^teacher/$', teacher.test, name='auth_teacher'),
+	url(r'^teacher/$', teacher.see, name='auth_teacher'),
+	url(r'^teacher/auth$', teacher.auth, name='auth_teacher'),
 ]
--- app/views/teacher.py
+++ app/views/teacher.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.http import HttpResponse
 
 def test(request):
 	return render(request, '../templates/teacher/test.html', {})
+
+def see(request):
+	return render(request , '../templates/teacher/auth.html', {})
+
+def auth(request):
+	user = authenticate(username='john', password='secret')
+	return HttpResponse(request.POST['email'])
+	#return render(request , '../templates/teacher/auth.html', {})
