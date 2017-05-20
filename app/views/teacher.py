734434a1da51730cdf8326b947a5dc9d74d3f22b
alex <levy_a@vanessa-mbp.home>
2017-05-20 17:02:53 +0200

    clean

--- app/views/teacher.py
+++ app/views/teacher.py
@@ -7,12 +7,12 @@ import pprint
 TEMPLATES_PATH  =  '../templates/teacher/'
 
 def see(request):
-	return render(request , TEMPLATES_PATH + 'auth.html', {})
+	return render(request, TEMPLATES_PATH + 'auth.html', {})
 
 def auth(request):
 	user = authenticate(username=request.POST['email'], password=request.POST['password'])
 	
-	if user is not None :
+	if user is not None:
 		#login is a auth method to set user in session
 		login(request, user)
 		return redirect('teacher_home')
@@ -21,7 +21,7 @@ def auth(request):
 	#TODO => redirect to auth view with bootstrap alert to say auth don t pass
 
 def home(request):
-	return render(request , TEMPLATES_PATH + 'home.html', {})
+	return render(request, TEMPLATES_PATH + 'home.html', {})
 
 def logout(request):
-	return render(request , TEMPLATES_PATH + 'auth.html', {})	
+	return render(request, TEMPLATES_PATH + 'auth.html', {})	
