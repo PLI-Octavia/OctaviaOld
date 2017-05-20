e434692c5734fb25426ca2aa62d7dc3c3ab34f3e
alex <levy_a@vanessa-mbp.home>
2017-05-20 15:50:58 +0200

    add templates path

--- app/urls.py
+++ app/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from .views import teacher
 
 urlpatterns = [
-	url(r'^teacher/$', teacher.see, name='login_teacher'),
-	url(r'^teacher/auth$', teacher.auth, name='auth_teacher'),
-	url(r'^teacher/home', teacher.home, name='home_teacher'),
-	url(r'^teacher/logout', teacher.home, name='logout_teacher'),
+	url(r'^teacher/$', teacher.see, name='teacher_login'),
+	url(r'^teacher/auth$', teacher.auth, name='teacher_auth'),
+	url(r'^teacher/home', teacher.home, name='teacher_home'),
+	url(r'^teacher/logout', teacher.home, name='teacher_logout'),
 ]
--- app/views/teacher.py
+++ app/views/teacher.py
@@ -3,21 +3,24 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 
+TEMPLATES_PATH  =  '../templates/teacher/'
+
 def see(request):
-	return render(request , '../templates/teacher/auth.html', {})
+	return render(request , TEMPLATES_PATH + 'auth.html', {})
 
 def auth(request):
 	user = authenticate(username=request.POST['email'], password=request.POST['password'])
 	
 	if user is not None :
+		#login is a auth method to set user in session
 		login(request, user)
-		return redirect('home_teacher')
+		return redirect('teacher_home')
 	else :
 		return HttpResponse('failed')
-	#return render(request , '../templates/teacher/auth.html', {})
+	#TODO => redirect to auth view with bootstrap alert to say auth don t pass
 
 def home(request):
-	return render(request , '../templates/teacher/home.html', {})
+	return render(request , TEMPLATES_PATH + 'home.html', {})
 
 def logout(request):
-	return render(request , '../templates/teacher/auth.html', {})	
+	return render(request , TEMPLATES_PATH + 'auth.html', {})	
