79c6f35a6cc9b0e17016d8e8b0df188e72f46cdd
Battello Théo <theo@smartnsoft.com>
2017-07-20 12:09:21 +0200

    Add game_id in URL and get game from DB.

--- app/urls.py
+++ app/urls.py
@@ -21,7 +21,7 @@ course_urls = [
 ]
 
 game_urls = [
-    url(r'^/?$', game.see, name='game'),
+    url(r'^(?P<game_id>\d+)/?$', game.see, name='game'),
 ]
 
 urlpatterns = [
--- app/views/game.py
+++ app/views/game.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
+from app.models import Game
 
 TEMPLATES_PATH = 'game/'
 
 
-def see(request):
-    # TODO: Dynamize
-    game = {"game_name": "Maths",
-            "game_path": "maths"}
-    return render(request, TEMPLATES_PATH + 'show.html', game)
+def see(request, game_id):
+    game = Game.objects.get(pk=game_id)
+
+    # Game folder name in static/games must be the game name.
+    return render(request, TEMPLATES_PATH + 'show.html', {"game_name": game.name})
