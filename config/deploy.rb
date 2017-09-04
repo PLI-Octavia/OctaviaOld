594d7addde69a564206a84ad79e2bd24e9f1dab2
Augustin Derupti <poney@poney.rocks>
2017-09-04 23:57:09 +0200

    ci: building with cap

---
+++ config/deploy.rb
@@ -0,0 +1,16 @@
+set :application, "octavia"
+set :branch, ENV['BRANCH'] || 'master'
+set :exclude_dir, [".svn", ".git", ".gitignore", "Capfile", "README.md", "readme.txt", "README.txt", "Changelog.txt", "CHANGELOG.txt", "CHANGELOG.md", "cache", "logs", "sessions"]
+set :include_dir, '.'
+# set :linked_dirs, ["var/logs","var/sessions","var/tmp","web/media","web/uploads"]
+set :log_level, :error
+set :repo_url, "git@github.com:PLI-Octavia/Octavia.git"
+set :scm, :copy
+set :stages, %w(Production)
+set :use_sudo, false
+
+namespace :deploy do
+  before :starting, :preparehosting
+  before :publishing, :install
+  before :finishing, :clean
+end
---
+++ config/deploy/Production.rb
@@ -0,0 +1,10 @@
+server ENV['DEPLOY_PRODUCTION_SERVER'], user: ENV['DEPLOY_PRODUCTION_USER'], roles: %w{app db web}
+if "#{fetch(:branch)}" == "master"
+  set :current_domain, "#{fetch(:application)}." + ENV['DEPLOY_PRODUCTION_DOMAIN']
+else
+  set :current_domain, "#{fetch(:application)}-#{fetch(:branch)}." + ENV['DEPLOY_PRODUCTION_DOMAIN']
+end
+set :default_domain, "#{fetch(:application)}." + ENV['DEPLOY_PRODUCTION_DOMAIN']
+set :default_deploy_to, "/webapps/octavia/#{fetch(:default_domain)}"
+set :deploy_to, "/webapps/octavia/#{fetch(:current_domain)}"
+set :keep_releases, 1
