52ebc2a50e3ae840534df074e67d8b56d736f504
Augustin Derupti <poney@poney.rocks>
2017-09-08 00:39:42 +0200

    ci: I also forget how to ssh. Don't ask question, it's not my job.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,12 +2,7 @@
 
 if [ "${BRANCH_NAME}" == "master" ]
 then
-	ssh octavia@51.255.166.37
-	cd /webapps/octavia/
-	sudo git pull
-	source envoctavia/bin/activate
-	python manage.py migrate
-	sudo /etc/mod_wsgi-express-80/apachectl stop && sudo /etc/mod_wsgi-express-80/apachectl start
+	ssh octavia@51.255.166.37 "cd /webapps/octavia/ && sudo git pull && source envoctavia/bin/activate && python manage.py migrate && sudo /etc/mod_wsgi-express-80/apachectl stop && sudo /etc/mod_wsgi-express-80/apachectl start"
 else
 	echo "Deploy to prod only on master"
 fi
