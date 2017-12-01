01d7a8afa0d813cf5a3d6e58d91bb013aa681c38
Augustin Derupti <poney@poney.rocks>
2017-12-01 01:49:51 +0100

    correcting tests in CI

--- bin/install.sh
+++ bin/install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+python3 -m venv envoctavia
+pip install Django
+
 echo $0
--- bin/test.sh
+++ bin/test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-python3 -m venv envoctavia
 source envoctavia/bin/activate
 python manage.py test
