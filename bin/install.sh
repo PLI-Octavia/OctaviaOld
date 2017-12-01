b81be9f55bc443e8aed8dd1f91c460e4dea10ee8
Augustin Derupti <poney@poney.rocks>
2017-12-01 02:06:18 +0100

    correcting tests in CI

--- bin/install.sh
+++ bin/install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+LOCALDIR=$(dirname "$0")
+cd "${LOCALDIR}"/.. || exit
+
 python3 -m venv envoctavia
 source envoctavia/bin/activate
-pip install Django
+python envoctavia/bin/pip install --upgrade pip
+python envoctavia/bin/pip install -r requirements.txt
+deactivate
 
 echo $0
--- bin/test.sh
+++ bin/test.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+LOCALDIR=$(dirname "$0")
+cd "${LOCALDIR}"/.. || exit
+
 source envoctavia/bin/activate
 python manage.py test
+deactivate
