ca07269200f00bedfc8f81fe95456934132ad025
Augustin Derupti <poney@poney.rocks>
2018-05-18 17:08:51 +0200

    CI: new environment & prod

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,7 +2,14 @@
 
 if [ "${BRANCH_NAME}" == "master" ]
 then
-	ssh octavia@51.255.166.37 "cd /webapps/octavia/ && sudo git pull && source envoctavia/bin/activate && python manage.py migrate && sudo /etc/mod_wsgi-express-80/apachectl stop && sudo /etc/mod_wsgi-express-80/apachectl start"
+	# ssh octavia@51.255.166.37 "cd /webapps/octavia/ && sudo git pull && source envoctavia/bin/activate && python manage.py migrate && sudo /etc/mod_wsgi-express-80/apachectl stop && sudo /etc/mod_wsgi-express-80/apachectl start"
+	
+	cd /home/octavia/current
+	sudo git pull
+	source envoctavia/bin/activate
+	python manage.py migrate
+	yes | python manage.py collectstatic
+	sudo service gunicorn restart
 else
 	echo "Deploy to prod only on master"
 fi
--- bin/install.sh
+++ bin/install.sh
@@ -5,8 +5,11 @@ cd "${LOCALDIR}"/.. || exit
 
 python3 -m venv envoctavia
 source envoctavia/bin/activate
+# pip install gunicorn
 python envoctavia/bin/pip install --upgrade pip
 python envoctavia/bin/pip install -r requirements.txt
+# yes | python manage.py migrate
+# yes | python manage.py collectstatic
 deactivate
 
 echo $0
