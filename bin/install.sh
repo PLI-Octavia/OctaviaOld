53c78cc3da9cdf2e6f01f58999ae6d72d37b9ab8
AugustinAdFab <augustin.derupti@adfab.fr>
2018-05-24 16:24:32 +0200

    ci: fixing tests

--- bin/install.sh
+++ bin/install.sh
@@ -8,7 +8,7 @@ source envoctavia/bin/activate
 # pip install gunicorn
 python envoctavia/bin/pip install --upgrade pip
 python envoctavia/bin/pip install -r requirements.txt
-# pip install psycopg2 --upgrade
+pip install psycopg2 --upgrade
 sed "s/'HOST': 'localhost'/'HOST': ''/g" octavia/settings.py
 python manage.py migrate
 # yes | python manage.py collectstatic
