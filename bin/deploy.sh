6d2c30e41e644921a97ddd2884e9aba49835f4db
SaintMarcellin <derupt_a@etna-alternance.net>
2018-07-04 11:33:21 +0200

    preprod a l'arrache

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -12,6 +12,16 @@ then
 	python manage.py collectstatic --noinput ## --clear
 	exit
 	sudo service gunicorn restart
+elif [ "${BRANCH_NAME}" == "develop" ]
+then
+	cd /home/octaviadev/current
+	su octaviadev
+	sudo git pull
+	source envoctavia/bin/activate
+	python manage.py migrate
+	python manage.py collectstatic --noinput ## --clear
+	exit
+	sudo service gunicorn-dev restart
 else
 	echo "Deploy to prod only on master"
 fi
